37bee6670a93bd8142704606afb55404684bf8b9
rjtests <rjaime.contact@gmail.com>
2025-03-05 10:53:24 +0100

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -1,1 +1,11 @@
+def multiply(data):
+    """Function that summarized cols a and b"""
 
+    data["sum"] = data["a"] * data["b"]
+
+    return data
+
+
+if __env:  # indicates we are running on Taktile
+    data = multiply(data)
+  
